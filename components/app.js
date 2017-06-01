c0c614da08d140a77b19a6a72f86372824a42f26
mischa-saunders <mischa.saunders@gmail.com>
2017-06-01 23:28:25 +1200

    add blackboard

--- components/app.js
+++ components/app.js
@@ -10,21 +10,30 @@ class App extends Component {
   render () {
     return <div>
       <Typist
-        avgTypingSpeed={200}
-        startDelay={0}>
-        <div className='box'>
-          <h1>Hi! I'm Mischa</h1>
-          <h2>I am at the foremost a person.</h2>
-          <h2>Feel free to get in touch</h2>
+        avgTypingDelay={80}
+        startDelay={0}
+        cursor={{show: false}}
+      >
+        <div className='ADifferentColour'>
+          <div className='textDiv'>
+            <div className='boxLeft'>
+              <h1>Hi I'm Mischa</h1>
+              <h2>I am, generally speaking, a person.</h2>
+              <h2>Feel free to get in touch</h2>
+            </div>
+            <div className='boxRight'>
+              <h2>Some links I want to follow up on for building this are:</h2>
+              <ol className='resourcesToFollowUpOn'>
+                <h3><li><a href='https://github.com/jstejada/react-typist'> Typist</a></li></h3>
+                <h3><li><a href='https://medium.freecodecamp.com/how-to-build-animated-microinteractions-in-react-aab1cb9fe7c8'>Animated Microinteractions </a></li></h3>
+                <h3><li><a href='https://github.com/FormidableLabs/react-animations'> React-Animations</a></li></h3>
+                <h3><li><a href='https://developer.mozilla.org/en-US/docs/Web/CSS/CSS_Transitions/Using_CSS_transitions'> CSS Transitions</a></li></h3>
+                <h3><li><a href='https://css-tricks.com/snippets/css/keyframe-animation-syntax/'> CSS Animations</a></li></h3>
+                <h3><li><a href='https://uxplanet.org/microinteractions-the-secret-to-great-app-design-4cfe70fbaccf'> Microinteractions: The Secret of Great App Design</a></li></h3>
+              </ol>
+            </div>
+          </div>
         </div>
-        <h2>Some links I want to follow up on for building this are:</h2>
-        <ol className='resourcesToFollowUpOn'>
-          <h3><li><a href='https://github.com/jstejada/react-typist'>Typist</a></li></h3>
-          <h3><li><a href='https://medium.freecodecamp.com/how-to-build-animated-microinteractions-in-react-aab1cb9fe7c8'>Animated Microinteractions </a></li></h3>
-          <h3><li><a href='https://github.com/FormidableLabs/react-animations'> React-Animations</a></li></h3>
-          <h3><li><a href='https://developer.mozilla.org/en-US/docs/Web/CSS/CSS_Transitions/Using_CSS_transitions'>CSS Transitions</a></li></h3>
-          <h3><li><a href='https://uxplanet.org/microinteractions-the-secret-to-great-app-design-4cfe70fbaccf'>Microinteractions: The Secret of Great App Design</a></li></h3>
-        </ol>
       </Typist>
     </div>
   }
